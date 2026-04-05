e3b6f0dea7dd20e2042752c5b45d59f8124e2d8f
aimy1 <aimy1@users.noreply.github.com>
2026-04-05 11:54:51 +0800

    Fix: Synchronize binary names, versions, and branding to Marka (v1.0.3)

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:minimal_markdown/main.dart';
+import 'package:marka/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
